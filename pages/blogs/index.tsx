02a47de7d6a3ebb97c7fe19f88a05cd6c2584b16
Hamidreza maharlu <maharlu.hamidreza@gmail.com>
2023-04-20 13:55:52 +0200

    fix/delete call api from getstaticprops

---
+++ pages/blogs/index.tsx
@@ -0,0 +1,26 @@
+import {FC} from "react";
+import BlogCard from "@/components/BlogCard";
+import {PostAPIResponse, readPostData} from "@/pages/api/post";
+import {InferGetStaticPropsType} from "next";
+
+
+
+
+export const getStaticProps = async () => {
+    const data = readPostData()
+    return {
+        props: {data}
+    }
+}
+
+type Props =InferGetStaticPropsType<typeof getStaticProps>
+
+const Index: FC<Props> = ({data}): JSX.Element => {
+    return (
+        <div className="max-w-3xl p-5 space-y-2 mx-auto">
+            {Array.isArray(data) && data.map((item,index)=><BlogCard key={index} title={item.title} description={item.meta} slug={item.slug}/>)}
+        </div>
+    );
+};
+
+export default Index;
